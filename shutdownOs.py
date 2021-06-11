ad8f5fcfa4ce490f55f361f058df34c79ac5fbcc
kuuen <42200391+kuuen@users.noreply.github.com>
2021-06-11 14:51:56 +0900

    Update shutdownOs.py

--- shutdownOs.py
+++ shutdownOs.py
@@ -5,15 +5,11 @@ import sys
 
 #from tkinter import messagebox
 
-
 def shutdown(args):
   subprocess.call("shutdown /s /t 3 /f /d p:4:1", shell=True)
 
-
   # メッセージボックス（情報） 
   #messagebox.showinfo('確認', "取得温度" + str(temp))
-
-
 
   file = open("C:/Users/XXX/Desktop/ShutdownValue.txt", "w", encoding = "utf_8")
   file.writelines(args[1] + "\n")
